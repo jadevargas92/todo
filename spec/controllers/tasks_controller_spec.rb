a49c6156f5a4a9a22104356636f612e33a6f1dc8
Jade Vargas <jadevargas92@gmail.com>
2017-05-17 04:29:08 +0000

    finished todo app

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,10 +5,15 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
 	    task1 = FactoryGirl.create(:task)
 	    task2 = FactoryGirl.create(:task)
+	    task1.update_attributes(title: "Something else")
 	    get :index
 	    expect(response).to have_http_status :success
 	    response_value = ActiveSupport::JSON.decode(@response.body)
       	expect(response_value.count).to eq(2)
+      	response_ids = response_value.collect do |task|
+	        task["id"]
+	      end
+	    expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
@@ -19,6 +24,16 @@ RSpec.describe TasksController, type: :controller do
 		expect(response).to have_http_status(:success)
       	task.reload
       	expect(task.done).to eq(true)
+    end
+  end
+
+  describe "task#create" do
+    it "should allow new tasks to be created" do
+    	post :create, task: {title: "Fix things"}
+    	expect(response).to have_http_status(:success)
+	    response_value = ActiveSupport::JSON.decode(@response.body)
+	    expect(response_value['title']).to eq("Fix things")
+	    expect(Task.last.title).to eq("Fix things")
     end
   end
 
